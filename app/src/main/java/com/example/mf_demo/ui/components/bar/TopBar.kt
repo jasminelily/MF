696c4d8a4d8693533106b3c15f37987f2fb1a0de
Lili Tan <iamlilyt@gmail.com>
2025-08-01 06:47:43 +0900

    change folder path

--- app/src/main/java/com/example/mf_demo/ui/components/bar/TopBar.kt
+++ app/src/main/java/com/example/mf_demo/ui/components/bar/TopBar.kt
@@ -1,15 +1,9 @@
-package com.example.mf_demo.ui.components
+package com.example.mf_demo.ui.components.bar
 
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Settings
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.res.stringResource
-import com.example.mf_demo.R
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
